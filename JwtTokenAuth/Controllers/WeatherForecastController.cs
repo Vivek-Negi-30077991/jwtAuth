2bb7ad534f8a39588da49e3db34f019c4426ae4b
Vivek-Negi-30077991 <vivek.negi@adani.com>
2023-05-15 07:14:48 +0530

    wrong commit

--- JwtTokenAuth/Controllers/WeatherForecastController.cs
+++ JwtTokenAuth/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace JwtTokenAuth.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
